2b64e5cf552ece3a1c44739b667cdbd61291722d
Neil Dwyer <neildwyer1991@gmail.com>
2023-08-19 20:13:08 -0700

    First commit

--- agent/agents/agent.py
+++ agent/agents/agent.py
@@ -1,9 +1,19 @@
 import livekit
+from typing import TypedDict
+from typing_extensions import Unpack
 
 class Agent:
-    def __init__(self, participant: livekit.LocalParticipant):
+    def __init__(self, *args, participant: livekit.LocalParticipant, room: livekit.Room):
         self.participant = participant
+        self.room = room
+        self.room.on("participant_connected", self._on_participant_connected_or_disconnected)
+        self.room.on("participant_disconnected", self._on_participant_connected_or_disconnected)
+        self.participants = self.room.participants
+
         self.initialize()
+
+    async def cleanup(self):
+        await self.room.disconnect()
 
     def initialize(self):
         raise NotImplementedError
@@ -16,3 +26,18 @@ class Agent:
 
     def should_process(self, track: livekit.Track, participant: livekit.Participant) -> bool:
         raise NotImplementedError
+
+    def on_participants_changed(self, participants: [livekit.Participant]):
+        pass
+
+    def _on_participant_connected_or_disconnected(self, *args):
+        self.participants = self.room.participants
+        self.on_participants_changed(self.participants)
+
+    
+
+    def has_non_agent_participants(self) -> bool:
+        for participant in self.room.participants:
+            if participant != self.participant:
+                return True
+        return False
--- agent/main.py
+++ agent/main.py
@@ -1,29 +1,49 @@
 import os
-import threading
 import livekit
-from flask import Flask
-from flask_cors import CORS
+import aiohttp
+import aiohttp_cors
+import dotenv
 
-app = Flask(__name__)
-CORS(app)
+from agents.transcription import Transcription
+from agents.agent import Agent
+
+dotenv.load_dotenv()
 
 
-LIVEKIT_WS_URL = os.environ.get("LIVEKIT_WS_URL", "")
-LIVEKIT_API_KEY = os.environ.get("LIVEKIT_API_KEY", "")
-LIVEKIT_API_SECRET = os.environ.get("LIVEKIT_API_SECRET", "")
+TOKEN_SERVICE_URL = os.environ.get("TOKEN_SERVICE_URL", "")
 PORT = 8000
 
-ROOM_AGENTS: {str: []} = {}
+AGENTS: [Agent] = []
 
+async def add_agent(request):
+    data = await request.json()
+    agent_type = data['agent']
+    ws_url = ''
+    token = ''
+    async with aiohttp.ClientSession() as session:
+        endpoint = f'{TOKEN_SERVICE_URL}/api/agent_connection_details?agent_type={agent_type}'
 
-@app.route("/add_agent", methods=["POST"])
-def add_agent(request):
-    data = request.get_json()
-    room = data['room']
-    agent = data['agent']
-    return {"success": True}
+        async with session.get(endpoint) as response:
+            json_response = await response.json()
+            ws_url = json_response['ws_url']
+            token = json_response['token']
+
+    room = livekit.Room()
+    if agent_type == 'transcription':
+        print('A transcription agent is being added')
+        await room.connect(ws_url, token)
+        participant = room.local_participant
+        transcription = Transcription(participant=participant, room=room)
+        AGENTS.append(transcription)
+
+    return aiohttp.web.json_response({'success': True})
+
+app = aiohttp.web.Application()
+app.add_routes([aiohttp.web.post('/add_agent', add_agent)])
+cors = aiohttp_cors.setup(app, defaults={"*": aiohttp_cors.ResourceOptions(
+    allow_credentials=True, expose_headers="*", allow_headers="*")})
+for route in list(app.router.routes()):
+    cors.add(route)
 
 if __name__ == "__main__":
-    print("hello world")
-    app.run(host="0.0.0.0", port=PORT, debug=True)
-
+    aiohttp.web.run_app(app, port=PORT, host='0.0.0.0')
