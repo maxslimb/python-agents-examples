32673d9b2aa358ff69478ad1448b9103fab6b422
Neil Dwyer <neildwyer1991@gmail.com>
2023-08-25 16:36:39 -0700

    more work on agent base class

--- agent/agents/agent.py
+++ agent/agents/agent.py
@@ -2,12 +2,18 @@ import livekit
 from typing import TypedDict
 from typing_extensions import Unpack
 
+
 class Agent:
     def __init__(self, *args, participant: livekit.LocalParticipant, room: livekit.Room):
         self.participant = participant
         self.room = room
-        self.room.on("participant_connected", self._on_participant_connected_or_disconnected)
-        self.room.on("participant_disconnected", self._on_participant_connected_or_disconnected)
+        self.room.on("participant_connected",
+                     self._on_participant_connected_or_disconnected)
+        self.room.on("participant_disconnected",
+                     self._on_participant_connected_or_disconnected)
+        self.room.on("track_subscribed", self._on_track_subscribed)
+        self.room.on("track_unsubscribed", self._on_track_unsubscribed)
+        self.room.on("track_published", self._on_track_published)
         self.participants = self.room.participants
 
         self.initialize()
@@ -20,7 +26,7 @@ class Agent:
 
     def on_video_frame(self, track: livekit.Track, participant: livekit.Participant, frame: livekit.VideoFrame):
         raise NotImplementedError
-    
+
     def on_audio_frame(self, track: livekit.Track, participant: livekit.Participant, frame: livekit.AudioFrame):
         raise NotImplementedError
 
@@ -34,7 +40,19 @@ class Agent:
         self.participants = self.room.participants
         self.on_participants_changed(self.participants)
 
-    
+    def _on_track_published(self):
+        pass
+
+    def _on_track_subscribed(self, track: livekit.Track, publication: livekit.RemoteTrackPublication, participant: livekit.RemoteParticipant):
+        if publication.kind == livekit.TrackKind.VIDEO:
+            video_stream = livekit.VideoStream(track)
+            video_stream.on("video_frame", lambda frame: self.on_video_frame(track, participant, frame))
+        elif publication.kind == livekit.TrackKind.AUDIO:
+            audio_stream = livekit.VideoStream(track)
+            audio_stream.on("audio_frame", lambda frame: self.on_audio_frame(track, participant, frame))
+
+    def _on_track_unsubscribed(self, publication: livekit.RemoteTrackPublication, participant: livekit.RemoteParticipant):
+        pass
 
     def has_non_agent_participants(self) -> bool:
         for participant in self.room.participants:
